379d2e99d08d293441a942c8e19a60280ab357f8
ziyanfeng <ziyanfeng.c@gmail.com>
2016-07-30 11:08:15 -0400

    feat: add Queue Practice

---
+++ projects/technical_interview/Quizzes/Queue Practice.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as
+few lines as possible.
+Make sure you pass the test cases too!"""
+
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage.pop(0)
+
+
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test peek
+# Should be 1
+print q.peek()
+
+# Test dequeue
+# Should be 1
+print q.dequeue()
+
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print q.dequeue()
+# Should be 3
+print q.dequeue()
+# Should be 4
+print q.dequeue()
+q.enqueue(5)
+# Should be 5
+print q.peek()
