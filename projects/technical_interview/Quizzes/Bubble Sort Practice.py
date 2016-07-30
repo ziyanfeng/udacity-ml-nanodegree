a0886ed971c220b99157f6650c2a31a2defe20a7
ziyanfeng <ziyanfeng.c@gmail.com>
2016-07-30 19:48:38 -0400

    refactor: improve method 2 of Bubble Sort Practice

--- projects/technical_interview/Quizzes/Bubble Sort Practice.py
+++ projects/technical_interview/Quizzes/Bubble Sort Practice.py
@@ -5,30 +5,30 @@ Input a list with no duplicates.
 Output a sorted list."""
 
 
-# Method 1
-def bubblesort(array):
-    unsorted = True
-    while unsorted:
-        unsorted = False
-        for i in range(len(array) - 1):
-            if array[i] > array[i+1]:
-                unsorted = True
-                array[i], array[i+1] = array[i+1], array[i]
-    return array
-
-
-# # Method 2
+# # Method 1
 # def bubblesort(array):
-#     passnum = len(array) - 1
 #     unsorted = True
 #     while unsorted:
 #         unsorted = False
-#         for i in range(passnum):
+#         for i in range(len(array) - 1):
 #             if array[i] > array[i+1]:
 #                 unsorted = True
 #                 array[i], array[i+1] = array[i+1], array[i]
-#             passnum -= 1
 #     return array
+
+
+# Method 2
+def bubblesort(array):
+    passnum = len(array) - 1
+    unsorted = True
+    while unsorted:
+        unsorted = False
+        for i in range(passnum):
+            if array[i] > array[i+1]:
+                unsorted = True
+                array[i], array[i+1] = array[i+1], array[i]
+        passnum -= 1
+    return array
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
