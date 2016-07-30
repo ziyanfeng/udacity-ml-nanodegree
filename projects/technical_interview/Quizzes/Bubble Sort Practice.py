35c0905aa2de325ffa3e59baf5a711053e401987
ziyanfeng <ziyanfeng.c@gmail.com>
2016-07-30 19:40:45 -0400

    feat: add a new method in Bubble Sort Practice

--- projects/technical_interview/Quizzes/Bubble Sort Practice.py
+++ projects/technical_interview/Quizzes/Bubble Sort Practice.py
@@ -5,6 +5,7 @@ Input a list.
 Output a sorted list."""
 
 
+# Method 1
 def bubblesort(array):
     unsorted = True
     while unsorted:
@@ -14,6 +15,15 @@ def bubblesort(array):
                 unsorted = True
                 array[i], array[i+1] = array[i+1], array[i]
     return array
+
+
+# # Method 2
+# def bubblesort(array):
+#     for passnum in range(len(array) - 1, 0, -1):
+#         for i in range(passnum):
+#             if array[i] > array[i+1]:
+#                 array[i], array[i+1] = array[i+1], array[i]
+#     return array
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
