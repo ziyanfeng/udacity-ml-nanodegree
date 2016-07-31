05e0908c1c8ad9061d503702e7859b9be7f7f59d
ziyanfeng <ziyanfeng.c@gmail.com>
2016-07-30 20:26:57 -0400

    feat: add Merge Sort Practice

---
+++ projects/technical_interview/Quizzes/Merge Sort Practice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""Implement merge sort in Python.
+Input a list.
+Output a sorted list."""
+
+
+def mergesort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        result, mid = [], len(array) // 2
+        lefthalf = mergesort(array[:mid])
+        righthalf = mergesort(array[mid:])
+        while len(lefthalf) > 0 and len(righthalf) > 0:
+            if lefthalf[0] < righthalf[0]:
+                result.append(lefthalf[0])
+                lefthalf.pop(0)
+            else:
+                result.append(righthalf[0])
+                righthalf.pop(0)
+        result += lefthalf
+        result += righthalf
+        return result
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14, 3]
+print mergesort(test)
