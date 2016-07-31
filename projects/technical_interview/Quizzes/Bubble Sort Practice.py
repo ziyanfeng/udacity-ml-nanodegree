7ea6a1ad078aa35e058b5c60bd85816378fdc189
ziyanfeng <ziyanfeng.c@gmail.com>
2016-07-30 20:26:13 -0400

    docs: update comment

--- projects/technical_interview/Quizzes/Bubble Sort Practice.py
+++ projects/technical_interview/Quizzes/Bubble Sort Practice.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python
 
 """Implement bubble sort in Python.
-Input a list with no duplicates.
+Input a list.
 Output a sorted list."""
 
 
-# # Method 1
-# def bubblesort(array):
-#     unsorted = True
-#     while unsorted:
-#         unsorted = False
-#         for i in range(len(array) - 1):
-#             if array[i] > array[i+1]:
-#                 unsorted = True
-#                 array[i], array[i+1] = array[i+1], array[i]
-#     return array
-
-
-# Method 2
+# Method 1
 def bubblesort(array):
-    passnum = len(array) - 1
     unsorted = True
     while unsorted:
         unsorted = False
-        for i in range(passnum):
+        for i in range(len(array) - 1):
             if array[i] > array[i+1]:
                 unsorted = True
                 array[i], array[i+1] = array[i+1], array[i]
-        passnum -= 1
     return array
 
 
-test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+# # Method 2
+# def bubblesort(array):
+#     passnum = len(array) - 1
+#     unsorted = True
+#     while unsorted:
+#         unsorted = False
+#         for i in range(passnum):
+#             if array[i] > array[i+1]:
+#                 unsorted = True
+#                 array[i], array[i+1] = array[i+1], array[i]
+#         passnum -= 1
+#     return array
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14, 3]
 print bubblesort(test)
